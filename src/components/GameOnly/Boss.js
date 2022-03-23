69e4fe4dde9a14502284f8397d10dc772dd253e8
--replace-all <jonathonsmcclendon@gmail.com>
2022-03-23 14:27:16 -0500

    testing different boss classes

--- src/components/GameOnly/Boss.js
+++ src/components/GameOnly/Boss.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Boss extends React.Component {
   render() {
-    return <div id="boss" className="cave-witch hover center"></div>;
+    return <div id="boss" className="mistic-mountains-ram center "></div>;
   }
 }
 
