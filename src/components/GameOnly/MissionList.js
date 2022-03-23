85062236a6ecda211e24c4cda4f3f2d44418733e
--replace-all <jonathonsmcclendon@gmail.com>
2022-03-23 14:27:21 -0500

    Create MissionList.js

---
+++ src/components/GameOnly/MissionList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MissionList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Misison List</h1>
+        <ul></ul>
+      </div>
+    );
+  }
+}
