a438239273420d0a1d1231a1e5e858234621f66f
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-07 18:06:09 -0500

    Update MissionListItem.js

---
+++ src/components/GameOnly/MissionListItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MissionListItem extends React.Component{
+    render(){
+        return(
+            <li>
+                <h5>Misison Name</h5>
+                <p></p>
+            </li>
+        )
+    }
+}
