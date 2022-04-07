581f56349c74d9689327623c1ed4e8743328f0ad
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-07 18:06:17 -0500

    added props pass

--- src/components/GameOnly/Tank.js
+++ src/components/GameOnly/Tank.js
@@ -14,29 +14,27 @@ class Tank extends React.Component {
       <div id="tank-box">
         <div className="row">
           <div className="col-3">
-            <div className="berzerk"></div>
+            <div id="tauntButton" className="berzerk"></div>
           </div>
           <div className="col-6">
-            <div
-              onMouseLeave={(e) => this.jumpEnd(e)}
-              onMouseEnter={(e) => this.jump(e)}
-              id="tank"
-            ></div>
+            <div id="tank"></div>
           </div>
           <div className="col-3"></div>
         </div>
 
-        <h6>Tank Name</h6>
+        <h6>{this.props.name}</h6>
         <div className="progress">
           <div
             className="progress-bar bg-danger"
             role="progressbar"
-            style={{ width: "88%" }}
+            style={{
+              width: (this.props.health / this.props.maxHp) * 100 + "%",
+            }}
             aria-valuenow="88"
             aria-valuemin="0"
             aria-valuemax="100"
           >
-            88%
+            {this.props.health} / {this.props.maxHp}
           </div>
         </div>
       </div>
