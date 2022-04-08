e4c26b3b026acf9a4951f77f7aabb09c66e7a05b
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-07 20:10:55 -0500

    remove healer health

--- src/components/GameOnly/Healer.js
+++ src/components/GameOnly/Healer.js
@@ -13,30 +13,19 @@ class Healer extends React.Component {
         </div>
 
         <div id="healButton" className="heal"></div>
-
-        <h6>{this.props.name}</h6>
-        <div className="progress">
-          <div
-            className="progress-bar bg-danger"
-            role="progressbar"
-            style={{ width: (this.props.hp / this.props.maxHp) * 100 + "%" }}
-            aria-valuenow="88"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            {this.props.hp}/{this.props.maxHp}
-          </div>
-        </div>
-        <div className="progress">
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: (this.props.mp / this.props.maxMp) * 100 + "%" }}
-            aria-valuenow="88"
-            aria-valuemin="0"
-            aria-valuemax="100/100"
-          >
-            {this.props.mp}/{this.props.maxMp}
+        <div className="char-border">
+          <h6 style={{ color: "#fff" }}>{this.props.name}</h6>
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: (this.props.mp / this.props.maxMp) * 100 + "%" }}
+              aria-valuenow="88"
+              aria-valuemin="0"
+              aria-valuemax="100/100"
+            >
+              {this.props.mp}/{this.props.maxMp}
+            </div>
           </div>
         </div>
       </div>
