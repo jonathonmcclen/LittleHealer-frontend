e89b0dceef47ad10cd6af276575bf16f9f271e3c
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-10 18:23:21 -0500

    added home screen to start game

--- src/components/GameOnly/GameOver.js
+++ src/components/GameOnly/GameOver.js
@@ -1,13 +1,24 @@
 import YouWin from "../../assets/img/gameScreens/YouWin.png";
 import GameOverImg from "../../assets/img/gameScreens/GameOver.png";
+import PlayAgain from "../../assets/img/gameScreens/playAgain.png";
 
 const GameOver = (props) => {
   return (
     <div id="game-over-bg">
       {props.win ? (
-        <img src={YouWin} width="100%" style={{ marginTop: "50%" }} />
+        <>
+          <img src={YouWin} width="100%" style={{ marginTop: "30%" }} />
+          <a href="/game">
+            <img src={PlayAgain} width="100%" />
+          </a>
+        </>
       ) : (
-        <img src={GameOverImg} width="100%" style={{ marginTop: "50%" }} />
+        <>
+          <img src={GameOverImg} width="100%" style={{ marginTop: "30%" }} />
+          <a href="/game">
+            <img src={PlayAgain} width="100%" />
+          </a>
+        </>
       )}
     </div>
   );
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Play from "../../assets/img/gameScreens/Play.png";
+import LittleHealer from "../../assets/img/gameScreens/LittleHealer.png";
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to: Little Healer - Web</h1>
+      <div className="home-bg">
+        <img src={LittleHealer} width="100%" style={{ marginTop: "30%" }} />
+        <a href="/game">
+          <img src={Play} width="100%" />
+        </a>
       </div>
     );
   }
