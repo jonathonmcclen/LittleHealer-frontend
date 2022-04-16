d430116c44585e548f5d29facf0f69eee1b78e6d
--replace-all <jonathonsmcclendon@gmail.com>
2022-04-16 18:30:28 -0500

    Delete endpoints.js

--- src/api/Playlist/endpoints.js
+++
@@ -1,3 +0,0 @@
-export const allPlaylists = "http://localhost:3000/playlist";
-export const allGenres = ["http://localhost:3000/genre", "/playlists"];
-export const playlistsByGenre = ["http://localhost:3000/genre", "/playlists"];
